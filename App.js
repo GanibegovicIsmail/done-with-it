e560a0c51d8ac18ae7b77f946e83247fa9d5294e
Ismail Ganibegovic <ismail.ganibegovic.20@size.ba>
2023-10-09 17:14:18 +0200

    feat: safe area view simulation

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>First chaning some text</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Neki Random Tekst</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'lightblue',
+    paddingTop: 40
   },
 });
